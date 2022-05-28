f2d9325ce817c50963cd5f923a574050a3d6ae9b
Gloria Galić <gloriaa.galicgg@gmail.com>
2022-05-28 13:55:42 +0200

    oprema repository

---
+++ Zadaca03/Repository/OpremaRepos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadaca03.Models;
+using DBLayer;
+using System.Data.SqlClient;
+
+namespace Zadaca03.Repository
+{
+    public class OpremaRepos
+    {
+        public static Oprema GetOprema(int id)
+        {
+            Oprema oprema = null;
+            string sql = $"SELECT * FROM Oprema WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                oprema = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return oprema;
+        }
+
+        private static Oprema CreateObject(SqlDataReader reader)
+        {
+            int Id = int.Parse(reader["id"].ToString());
+            DateTime datum = DateTime.Parse(reader["DatumZaprimanja"].ToString());
+            string zaprimio = reader["Zaprimio"].ToString();
+            string izvor = reader["IzvorFinanciranja"].ToString();
+            string projekt = reader["NazivProjekta"].ToString();
+            string vrsta = reader["VrstaOpreme"].ToString();
+            string naziv = reader["NazivOpreme"].ToString();
+            string opis = reader["OpisOpreme"].ToString();
+
+            var oprema = new Oprema
+            {
+                id = Id,
+                DatumZaprimanja = datum,
+                Zaprimio = zaprimio,
+                IzvorFinanciranja = izvor,
+                NazivProjekta = projekt,
+                VrstaOpreme = vrsta,
+                NazivOpreme = naziv,
+                OpisOpreme = opis,
+
+            };
+            return oprema;
+        }
+    }
+}
